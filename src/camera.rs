147aa4a85359df1f2d8d558fe2e4e2d993f244db
Eerwig <er3r3r@protonmail.com>
2026-02-08 22:11:18 -0500

    Removed unnecessary camera stuff

--- src/camera.rs
+++ src/camera.rs
@@ -1,13 +1,7 @@
-use crate::AppState;
 use bevy::prelude::*;
 
 pub(super) fn plugin(app: &mut App) {
-    app.register_type::<CameraTarget>();
-    app.register_type::<CameraSmoothing>();
-    app.init_resource::<CameraTarget>();
-    app.init_resource::<CameraSmoothing>();
     app.add_systems(Startup, spawn_camera);
-    app.add_systems(PostUpdate, follow_camera.run_if(in_state(AppState::Main)));
 }
 
 #[derive(Resource, Default, Clone, Copy, Reflect)]
@@ -19,29 +13,5 @@ pub struct CameraTarget(Vec2);
 pub struct CameraSmoothing(f32);
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn((
-        Name::new("Camera"),
-        Camera2d,
-        Projection::Orthographic(OrthographicProjection {
-            scaling_mode: bevy::camera::ScalingMode::FixedVertical {
-                viewport_height: 150.0,
-            },
-            ..OrthographicProjection::default_2d()
-        }),
-    ));
-}
-
-fn follow_camera(
-    mut query: Query<&mut Transform, With<Camera2d>>,
-    target: Res<CameraTarget>,
-    smoothing: Res<CameraSmoothing>,
-) {
-    let Ok(mut camera) = query.single_mut() else {
-        return;
-    };
-
-    camera.translation = camera.translation.lerp(
-        vec3(target.0.x, target.0.y, camera.translation.z),
-        0.0 - smoothing.0,
-    );
+    commands.spawn((Name::new("Camera"), Camera2d));
 }
